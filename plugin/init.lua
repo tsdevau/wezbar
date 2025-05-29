39ce86decf146ec95390e1594276359d3178e9a2
Tim Stone <tim@tsdev.au>
2025-05-29 19:41:06 +1000

    refactor(config): simplify window padding and tab bar settings
    - enable fancy tab bar
    - remove explicit window padding settings
    - remove tab bar background colour override

--- plugin/init.lua
+++ plugin/init.lua
@@ -65,18 +65,13 @@ function M.setup(opts)
 end
 
 function M.apply_to_config(config)
-	config.use_fancy_tab_bar = false
+	config.use_fancy_tab_bar = true
 	config.show_new_tab_button_in_tab_bar = false
 	config.tab_max_width = 32
 	config.window_decorations = "RESIZE"
 	config.window_padding = config.window_padding or {}
-	config.window_padding.left = 0
-	config.window_padding.right = 0
-	config.window_padding.top = 0
-	config.window_padding.bottom = 0
 	config.colors = config.colors or {}
 	config.colors.tab_bar = config.colors.tab_bar or {}
-	config.colors.tab_bar.background = require("wezbar.config").theme.normal_mode.c.bg
 	config.status_update_interval = 500
 end
 
